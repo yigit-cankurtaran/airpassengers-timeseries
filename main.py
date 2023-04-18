32c77634b36b185722837156391a462c757a0771
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-04-18 23:48:58 +0300

    No errors, but very high MSE and the predictions is a straight line. Will look into it.

---
+++ main.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from statsmodels.tsa.arima.model import ARIMA
+
+#  import the flights dataset
+flights = sns.load_dataset('flights')
+
+#  convert the dataset to a time series
+flights.index = pd.DatetimeIndex(flights['year'].astype(
+    str) + '-' + flights['month'].astype(str), freq='MS')
+flights = flights['passengers']
+
+
+#  split the dataset into train and test
+train, test = train_test_split(flights, test_size=0.2, shuffle=False)
+
+#  fit the model
+model = ARIMA(train, order=(1, 1, 1))
+model_fit = model.fit()
+
+#  make predictions
+predictions = model_fit.predict(
+    start=len(train), end=len(train)+len(test)-1, dynamic=False)
+
+#  calculate the error
+error = mean_squared_error(test, predictions)
+print(f'Error: {error}')
+
+#  plot the results
+plt.plot(train, label='Train')
+plt.plot(test, label='Test')
+plt.plot(predictions, label='Predictions')
+plt.legend()
+plt.show()
